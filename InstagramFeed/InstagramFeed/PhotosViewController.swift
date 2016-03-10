2b6b53c4acc5970bf2949f8266a838ff37fe2f0f
Jeremie Lim <jeremielim@Jeremies-MBP.attlocal.net>
2016-03-09 22:41:33 -0800

    Finished Milestone 1

---
+++ InstagramFeed/InstagramFeed/PhotosViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotosViewController.swift
+//  InstagramFeed
+//
+//  Created by Jeremie Lim on 3/9/16.
+//  Copyright © 2016 Jeremie Lim. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
