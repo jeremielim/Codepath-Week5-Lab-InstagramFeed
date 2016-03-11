d1ba6820fec85a2644e596ce478ca60a126c2f2e
Jeremie Michaels Lim <hello@jeremiespoken.com>
2016-03-10 18:08:54 -0800

    Added milestone 3

--- InstagramFeed/InstagramFeed/PhotosViewController.swift
+++ InstagramFeed/InstagramFeed/PhotosViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AFNetworking
 
 class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var photos: [NSDictionary] = []
+    var photos: [NSDictionary]!
+    var names: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.rowHeight = 320
         tableView.delegate = self
         tableView.dataSource =  self
+        
+        photos = []
         
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=e05c462ebd86446ea48a5af73769b602")
         let request = NSURLRequest(URL: url!)
@@ -34,25 +38,42 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 if let data = data {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
-                            print("responseDictionary: \(responseDictionary)")
+
                             // Store the returned array of media in your photos property
                             self.photos = responseDictionary["data"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                           
                     }
                 }
         });
-        task.resume()
         
+        task.resume()
         
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return photos.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("PhotosCell")
         
-        return cell!
+        let cell = tableView.dequeueReusableCellWithIdentifier("PhotosCell") as! PhotosTableViewCell
+        
+        let photo = photos[indexPath.row]
+
+        let username = photo.valueForKeyPath("user.username") as! String
+        let urlString = photo.valueForKeyPath("images.low_resolution.url") as! String
+        
+        print(urlString)
+        
+        cell.cellImageView.setImageWithURL(NSURL(string: urlString)!)
+//        print(urlString)
+//        cell.cellImageView.setImageWithURL(NSURL(string: urlString));)
+        
+        
+        
+        return cell
     }
     
 
